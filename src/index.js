437915b0d0903ffef12c4fe07b0967a1b5e71514
littledev13 <irvandn13@gmail.com>
2022-06-27 20:01:55 +0700

    Ready to Code...

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(
+  <h1 className="text-sky-300 font-bold text-4xl">I'am Litte D</h1>,
+  root
+);
